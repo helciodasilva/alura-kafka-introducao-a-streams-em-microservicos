ff821aea8dd4c9bb42d3db801fde4993297f625f
helciodasilva <helciodasilva@gmail.com>
2020-10-29 17:02:46 -0300

    Aula 3: Criando nossa camada

---
+++ ecommerce/src/main/java/br/com/alura/ecommerce/ConsumerFunction.java
@@ -0,0 +1,9 @@
+package br.com.alura.ecommerce;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+public interface ConsumerFunction {
+
+    void consume(ConsumerRecord<String, String> record);
+
+}
